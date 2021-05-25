c9574143e0d0f86419402a17c3e5675bc05084a1
Gavin Young <younnit@gmail.com>
2021-05-25 16:40:34 -0600

    Worked on the header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Logo</h1>
+      <nav>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
