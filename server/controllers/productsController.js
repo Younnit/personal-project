1076c46b942cc1c97103e7dd17fa65d85d503513
Gavin Young <younnit@gmail.com>
2021-06-14 13:33:05 -0600

    products controller log

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -2,6 +2,7 @@ module.exports = {
     getTheProducts: (req, res) => {
         const db = req.app.get('db')
         db.products.get_the_products().then(products => {
+            console.log(products)
             res.status(200).send(products)
         }).catch(err => {
             console.log(err)
